f26bb14de63e1d7535ae721afcf26d59a9b8ceac
Ali Karimi <ajkarimi7@gmail.com>
2019-09-06 18:55:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,8 @@
 # Write your code here.
 
 def line(katz_deli_line)
-  if katz_deli_line.index != 0
-    katz_deli_line.index += 1 
+  if katz_deli_line.length != 0
+    katz_deli_line.length += 1 
     puts "You are currently #{katz_deli_line.index} in line"
   else 
     puts "The line is currently empty"
